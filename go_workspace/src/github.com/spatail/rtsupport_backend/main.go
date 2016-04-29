cce7064c8f72d4dfd6c0ffa8547e5eab13623f17
Saad Patail <spatail@gmail.com>
2016-04-29 14:07:56 -0700

    UI now talks to backend, need to address goroutine leak

--- go_workspace/src/github.com/spatail/rtsupport_backend/main.go
+++ go_workspace/src/github.com/spatail/rtsupport_backend/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	r "github.com/dancannon/gorethink"
+	"log"
 	"net/http"
 )
 
 type Channel struct {
-	Id   string `json:"id"` // field tag to specify special encoding, json is the package we are providing tagging for
-	Name string `json:"name"`
+	Id   string `json:"id" gorethink:"id,omitempty"` // field tag to specify special encoding, json is the package we are providing tagging for
+	Name string `json:"name" gorethink:"name"`
+}
+
+type User struct {
+	Id   string `gorethink:"id,omitempty"`
+	Name string `gorethink:"name"`
 }
 
 func main() {
-	router := NewRouter()
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "rtsupport",
+	})
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	router := NewRouter(session)
 
 	router.Handle("channel add", addChannel)
-
+	router.Handle("channel subscribe", subscribeChannel)
 	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
 }
